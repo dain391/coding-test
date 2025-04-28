ab4288ad485f4068b9102702112e8adedcdbfc66
dain391 <0901ekdls@gmail.com>
2025-04-28 10:42:13 +0900

    [level 0] Title: 두 수의 곱 구하기, Time: 0.03 ms, Memory: 90.5 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/120804. 두 수의 곱 구하기/두 수의 곱 구하기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int num1, int num2) {
+        int answer = 0;
+        if (0 <= num1 && num1 <= 100 && 0 <= num2 && num2 <= 100) {
+            answer = num1 * num2;
+        } else {
+            System.out.println("잘못된 숫자 입니다.");
+        }
+        return answer;
+    }
+}
