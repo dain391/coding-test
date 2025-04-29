427e608389b67bea43f80b824d4f62522ca25da5
이다인 <0901ekdls@gmail.com>
2025-04-29 09:06:33 +0900

    [level 0] Title: 몫 구하기, Time: 0.03 ms, Memory: 75.9 MB -BaekjoonHub

--- Java/프로그래머스/0/120805. 몫 구하기/몫 구하기.java
+++ Java/프로그래머스/0/120805. 몫 구하기/몫 구하기.java
@@ -1,7 +1,7 @@
 class Solution {
     public int solution(int num1, int num2) {
         int answer = 0;
-        if (-50000 <= num1 && num1 <= 50000 && -50000 <= num2 && num2 <= 50000) {
+        if (0 < num1 && num1 <= 100 && 0 < num2 && num2 <= 100) {
             answer = num1 / num2;
         } else {
             System.out.println("잘못된 숫자 입니다.");
