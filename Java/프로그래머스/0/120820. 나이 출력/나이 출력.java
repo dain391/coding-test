651ff88c92c4ce99ab322161d3e61bb42d10e37c
이다인 <0901ekdls@gmail.com>
2025-04-29 09:31:26 +0900

    [level 0] Title: 나이 출력, Time: 10.81 ms, Memory: 77.9 MB -BaekjoonHub

--- Java/프로그래머스/0/120820. 나이 출력/나이 출력.java
+++ Java/프로그래머스/0/120820. 나이 출력/나이 출력.java
@@ -1,12 +1,10 @@
 class Solution {
     public int solution(int age) {
-        if (age <= 0 || age > 120) {
-            System.out.println("잘못된 나이입니다.");
-            return 0;
-        }
-
-        int answer = 2022 - age + 1;
-        System.out.println("2022년 기준 " + age + "살이므로 " + answer + "년생입니다.");
+        if (0 < age && age <=120)
+        int answer = 0;
+        answer = 2022 - age + 1
+            
+        System.out.println("2022년 기준 " + age + "살이므로 " + answer + "년생입니다.")
         return answer;
     }
 }
